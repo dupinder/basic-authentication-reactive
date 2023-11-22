05f24f2325af4e45c380e74c0ed1841c76df31d7
Dupinder <dupinderdhiman5@gmail.com>
2023-11-22 20:02:19 +0530

    chore:  add deps related to connection factory of r2dbc for mssql and h2 db with flyway deps
    # r2dbc dependencies for MSSQL and h2 db
    # add flyway config for mssql

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,16 +25,27 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+	implementation("org.springframework.data:spring-data-r2dbc:3.1.5")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-sqlserver")
-	compileOnly("org.projectlombok:lombok")
+
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
-	runtimeOnly("io.r2dbc:r2dbc-h2")
-	runtimeOnly("io.r2dbc:r2dbc-mssql:1.0.0.RELEASE")
-	annotationProcessor("org.projectlombok:lombok")
+
+	implementation("io.r2dbc:r2dbc-mssql:1.0.2.RELEASE")
+	implementation("io.r2dbc:r2dbc-h2:1.0.0.RELEASE")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	implementation("org.flywaydb:flyway-core:9.22.3")
+	implementation("org.flywaydb:flyway-sqlserver:9.22.3")
 }
 
 tasks.withType<Test> {
